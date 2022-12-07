02f26951de807d1bb2409a8c0f4ec2ab6550ac71
César Sáinz <cesarsainz23@hotmail.com>
2022-12-07 12:38:18 -0600

    Changed functions in order to get data easier
    you don't have to refresh the page to refresh data

--- src/App.js
+++ src/App.js
@@ -2,17 +2,44 @@ import './App.css';
 import divisor from './images/pattern-divider-desktop.svg';
 import divisorTwo from './images/pattern-divider-mobile.svg';
 import diceIcon from './images/icon-dice.svg';
-import AdviceGenerator from './DataFetching.js';
-import SeparatingData from './SeparatingData.js';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  function getUrl(){
+    return 'https://api.adviceslip.com/advice/' + getRandomInt(224).toString()
+  }
+
+
+  const[advice,setAdvice] = useState('');
+  const[id,setId]= useState('');
+  const[update,setUpdate] = useState(0);
+
+  useEffect(() =>{
+    setTimeout(axios.get(getUrl())
+    .then((response)  => {
+      console.log(response.data)
+      setAdvice(response.data.slip.advice)
+      setId(response.data.slip.id)
+      })
+    .catch(err => {
+      console.log(err)
+      }));
+  },[update]
+  )
+  
   return (
     
     <div className="App">
       <header className="App-header"> 
         <div className="Advice-box" style={{backgroundColor:'#313A49'}}>
-          <div className = "Advice-title">Advice #{SeparatingData("id")}</div>
-          <div className = "Advice-content">{SeparatingData("advice")}</div>
+          <div className = "Advice-title">Advice #{id}</div>
+          <div className = "Advice-content">{advice}</div>
           <div className = "Advice-separator">
             <picture>
               <source media="(max-width: 450px)" alt='separator-pic' srcSet={divisorTwo} />
@@ -20,7 +47,7 @@ function App() {
             </picture>
           </div>
           <div className = 'Advice-dice-box'>
-            <div className = "Advice-dice" onClick={()=>window.location.reload(false)}>
+            <div className = "Advice-dice" onClick={()=>setUpdate(update+1)}>
             <img alt='dice-icon' className = "Dice-icon" src={diceIcon}/>
             </div>
           </div>
---
+++ src/RandomNumberGenerator.js
@@ -0,0 +1,11 @@
+function RandomNumberGenerator(max){
+   let randomNumber =  Math.floor(Math.random() * max);
+   return(
+    randomNumber
+    );
+
+}
+
+
+
+export default RandomNumberGenerator
